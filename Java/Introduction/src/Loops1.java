be60805837c15f85f12c338f02abbc8813e83a67
Riger Vivien <vivri66@gmail.com>
2022-01-02 22:38:25 +0100

    loops1 done

--- Java/Introduction/src/Loops1.java
+++ Java/Introduction/src/Loops1.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Loops1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+    }
 }
